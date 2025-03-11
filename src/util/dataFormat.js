cbbccd77295d9fc9e716cdcf79c67bc3c87241ee
des321321 <des321321@naver.com>
2025-03-11 14:30:37 +0900

    시간 표시

--- src/util/dataFormat.js
+++ src/util/dataFormat.js
@@ -1,7 +1,9 @@
 export function reductionDate(date) {
-	const year = date.getFullYear().toString().slice(2);
-	const month = (date.getMonth() + 1).toString().padStart(2, '0');
-	const day = date.getDate().toString().padStart(2, '0');
-  
-	return `${year}.${month}.${day}`;
+  const year = date.getFullYear().toString().slice(2); // 년도
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 월
+  const day = date.getDate().toString().padStart(2, '0'); // 일
+  const hours = date.getHours().toString().padStart(2, '0');  // 시간
+  const minutes = date.getMinutes().toString().padStart(2, '0');  // 분
+
+  return `${year}.${month}.${day} ${hours}:${minutes}`;
 }
